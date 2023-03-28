185e4a6800671d37dffa99caa5a4feeaa1837e44
lesotropyx <90562367+lesotropyx@users.noreply.github.com>
2023-03-28 13:44:01 +0300

    laboratornaya0 Kohanskaya Alena

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,6 +1,8 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
-
+#include <math.h>
+#include <iostream>
 int64_t power(int64_t x, uint16_t n) {
-    return 0;
+    std::cout << pow(x, n);
+    return pow(x, n);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
+
+
+
 int main() {
+    power(2, 3);
     return 0;
 }
